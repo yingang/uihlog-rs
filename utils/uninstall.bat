d498bc84a09403b8ed1f75091810f789b45dbce7
Gang Yin <1246410+yingang@users.noreply.github.com>
2022-07-02 16:41:25 +0800

    update uninstall script

--- utils/uninstall.bat
+++ utils/uninstall.bat
@@ -3,3 +3,4 @@
 reg delete "HKEY_CURRENT_USER\Software\Classes\.uihlog" /f
 reg delete "HKEY_CURRENT_USER\Software\Classes\uihlog_auto_file" /f
 reg delete "HKEY_CURRENT_USER\Software\Classes\Directory\shell\unUIHlog" /f
+reg delete "HKEY_CURRENT_USER\Software\Classes\Directory\shell\unUIHlog-PID" /f
